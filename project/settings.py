e000bed4b581826d94a1bade0149dab92510e3d0
Abdou <babana9977@gmail.com>
2024-08-07 03:41:33 +0000

    second commit

--- project/settings.py
+++ project/settings.py
@@ -32,7 +32,7 @@ EMAIL_PORT=587
 EMAIL_USE_TLS=True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 CELERY_BEAT_SCHEDULE = {
     'create-courses-every-20-hours': {
         'task': 'GPP.tasks.generate_courses_task',
